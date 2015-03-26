5d6a92e701b4dbe0597f811ad357b45385cf3ef4
Lubomir Rintel <lkundrak@v3.sk>
2015-03-26 17:52:04 +0100

    libnm,device: don't notify of property change when getting vendor & product from udev
    I have no idea what was the purpose, however this causes an infinite loop if
    udev has not product & vendor and the notify handler gets the property.

--- libnm/nm-device.c
+++ libnm/nm-device.c
@@ -1428,7 +1428,6 @@ nm_device_get_product (NMDevice *device)
 			/* Sometimes ID_PRODUCT_FROM_DATABASE is used? */
 			priv->product = _get_udev_property (device, "ID_MODEL_ENC", "ID_PRODUCT_FROM_DATABASE");
 		}
-		_nm_object_queue_notify (NM_OBJECT (device), NM_DEVICE_PRODUCT);
 	}
 	return priv->product;
 }
@@ -1450,10 +1449,8 @@ nm_device_get_vendor (NMDevice *device)
 	g_return_val_if_fail (NM_IS_DEVICE (device), NULL);
 
 	priv = NM_DEVICE_GET_PRIVATE (device);
-	if (!priv->vendor) {
+	if (!priv->vendor)
 		priv->vendor = _get_udev_property (device, "ID_VENDOR_ENC", "ID_VENDOR_FROM_DATABASE");
-		_nm_object_queue_notify (NM_OBJECT (device), NM_DEVICE_VENDOR);
-	}
 	return priv->vendor;
 }
 
