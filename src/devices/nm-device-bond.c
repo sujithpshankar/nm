sujithshankar/NetworkManager

core: let device plugins advertise supported link and setting types
Instead of looping over all plugins and asking each plugin whether it
can handle a link or a connection, have them advertise the link and
connection types they support, and use that when creating new devices.

src/devices/nm-device-bond.c
@@ -591,7 +591,9 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 		                              NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(BOND, Bond, bond,
+DEFINE_DEVICE_FACTORY_INTERNAL      (BOND, Bond, bond,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_BOND)
+	DEFINE_FACTORY_SETTING_TYPES (NM_SETTING_BOND_SETTING_NAME),
 	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)

src/devices/nm-device-bridge.c
@@ -552,7 +552,9 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(BRIDGE, Bridge, bridge,
+DEFINE_DEVICE_FACTORY_INTERNAL      (BRIDGE, Bridge, bridge,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_BRIDGE)
+	DEFINE_FACTORY_SETTING_TYPES (NM_SETTING_BRIDGE_SETTING_NAME),
 	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)

src/devices/nm-device-vlan.c
@@ -692,8 +692,10 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	return device;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(VLAN, Vlan, vlan, \
-	factory_iface->new_link = new_link; \
+DEFINE_DEVICE_FACTORY_INTERNAL      (VLAN, Vlan, vlan,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_VLAN)
+	DEFINE_FACTORY_SETTING_TYPES (NM_SETTING_VLAN_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)
 

sujithpshankar/nm

src/devices/nm-device-bond.c
@@ -598,7 +598,9 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 		                              NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(BOND, Bond, bond,
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (BOND, Bond, bond,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_BOND)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_BOND_SETTING_NAME),
 	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)

src/devices/nm-device-bridge.c
@@ -539,7 +539,9 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(BRIDGE, Bridge, bridge,
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (BRIDGE, Bridge, bridge,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_BRIDGE)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_BRIDGE_SETTING_NAME),
 	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)

src/devices/nm-device-vlan.c
@@ -750,8 +750,10 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	return device;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(VLAN, Vlan, vlan, \
-	factory_iface->new_link = new_link; \
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (VLAN, Vlan, vlan,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_VLAN)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_VLAN_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)
 