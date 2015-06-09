1f85bc59f64f193ed5d3d07fde0dd006cac0f145
Thomas Haller <thaller@redhat.com>
2015-06-09 09:02:32 +0200

    config: add nm_config_parse_boolean() function
    Add function to parse as boolean according our NMConfig convention.
    Split this out from nm_config_keyfile_get_boolean() so that we can use
    it independently. Also, change the return type to gint, so that one might
    pass -1 to indicate an invalid/missing boolean value.

--- src/nm-config.h
+++ src/nm-config.h
@@ -95,11 +95,13 @@ NMConfig *nm_config_new (const NMConfigCmdLineOptions *cli, GError **error);
 NMConfig *nm_config_setup (const NMConfigCmdLineOptions *cli, GError **error);
 void nm_config_reload (NMConfig *config);
 
+gint nm_config_parse_boolean (const char *str, gint default_value);
+
 GKeyFile *nm_config_create_keyfile (void);
-gboolean nm_config_keyfile_get_boolean (GKeyFile *keyfile,
-                                        const char *section,
-                                        const char *key,
-                                        gboolean default_value);
+gint nm_config_keyfile_get_boolean (GKeyFile *keyfile,
+                                    const char *section,
+                                    const char *key,
+                                    gint default_value);
 GSList *nm_config_get_device_match_spec (const GKeyFile *keyfile, const char *group, const char *key);
 
 G_END_DECLS
